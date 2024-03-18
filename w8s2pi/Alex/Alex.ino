07e0973e928493318f39df6c03a8b09e5f5467e6
nusrpi <nusrpi2324@gmail.com>
2024-03-18 09:33:27 +0800

    yes

---
+++ w8s2pi/Alex/Alex.ino
@@ -0,0 +1,455 @@
+#include <serialize.h>
+#include <stdarg.h>
+
+#include "packet.h"
+#include "constants.h"
+
+volatile TDirection dir;
+
+/*
+ * Alex's configuration constants
+ */
+
+// Number of ticks per revolution from the 
+// wheel encoder.
+
+#define COUNTS_PER_REV      1
+
+// Wheel circumference in cm.
+// We will use this to calculate forward/backward distance traveled 
+// by taking revs * WHEEL_CIRC
+
+#define WHEEL_CIRC          19
+
+/*
+ *    Alex's State Variables
+ */
+
+// Store the ticks from Alex's left and
+// right encoders.
+volatile unsigned long leftForwardTicks; 
+volatile unsigned long rightForwardTicks;
+volatile unsigned long leftReverseTicks; 
+volatile unsigned long rightReverseTicks;
+
+// Left and right encoder ticks for turning
+
+volatile unsigned long leftForwardTicksTurn; 
+volatile unsigned long rightForwardTicksTurn;
+volatile unsigned long leftReverseTicksTurn; 
+volatile unsigned long rightReverseTicksTurn;
+
+// Store the revolutions on Alex's left
+// and right wheels
+volatile unsigned long leftRevs;
+volatile unsigned long rightRevs;
+
+// Forward and backward distance traveled
+volatile unsigned long forwardDist;
+volatile unsigned long reverseDist;
+
+
+/*
+ * 
+ * Alex Communication Routines.
+ * 
+ */
+ 
+TResult readPacket(TPacket *packet)
+{
+    // Reads in data from the serial port and
+    // deserializes it.Returns deserialized
+    // data in "packet".
+    
+    char buffer[PACKET_SIZE];
+    int len;
+
+    len = readSerial(buffer);
+
+    if(len == 0)
+      return PACKET_INCOMPLETE;
+    else
+      return deserialize(buffer, len, packet);
+    
+}
+
+void sendStatus()
+{
+  // Implement code to send back a packet containing key
+  // information like leftTicks, rightTicks, leftRevs, rightRevs
+  // forwardDist and reverseDist
+  // Use the params array to store this information, and set the
+  // packetType and command files accordingly, then use sendResponse
+  // to send out the packet. See sendMessage on how to use sendResponse.
+  //
+}
+
+void sendMessage(const char *message)
+{
+  // Sends text messages back to the Pi. Useful
+  // for debugging.
+  
+  TPacket messagePacket;
+  messagePacket.packetType=PACKET_TYPE_MESSAGE;
+  strncpy(messagePacket.data, message, MAX_STR_LEN);
+  sendResponse(&messagePacket);
+}
+
+void dbprintf(char *format, ...) {
+  va_list args; 
+  char buffer[128]; 
+
+  va_start(args, format); 
+  vsprintf(buffer, format, args); 
+  sendMessage(buffer); 
+  }
+
+void sendBadPacket()
+{
+  // Tell the Pi that it sent us a packet with a bad
+  // magic number.
+  
+  TPacket badPacket;
+  badPacket.packetType = PACKET_TYPE_ERROR;
+  badPacket.command = RESP_BAD_PACKET;
+  sendResponse(&badPacket);
+  
+}
+
+void sendBadChecksum()
+{
+  // Tell the Pi that it sent us a packet with a bad
+  // checksum.
+  
+  TPacket badChecksum;
+  badChecksum.packetType = PACKET_TYPE_ERROR;
+  badChecksum.command = RESP_BAD_CHECKSUM;
+  sendResponse(&badChecksum);  
+}
+
+void sendBadCommand()
+{
+  // Tell the Pi that we don't understand its
+  // command sent to us.
+  
+  TPacket badCommand;
+  badCommand.packetType=PACKET_TYPE_ERROR;
+  badCommand.command=RESP_BAD_COMMAND;
+  sendResponse(&badCommand);
+}
+
+void sendBadResponse()
+{
+  TPacket badResponse;
+  badResponse.packetType = PACKET_TYPE_ERROR;
+  badResponse.command = RESP_BAD_RESPONSE;
+  sendResponse(&badResponse);
+}
+
+void sendOK()
+{
+  TPacket okPacket;
+  okPacket.packetType = PACKET_TYPE_RESPONSE;
+  okPacket.command = RESP_OK;
+  sendResponse(&okPacket);  
+}
+
+void sendResponse(TPacket *packet)
+{
+  // Takes a packet, serializes it then sends it out
+  // over the serial port.
+  char buffer[PACKET_SIZE];
+  int len;
+
+  len = serialize(buffer, packet, sizeof(TPacket));
+  writeSerial(buffer, len);
+}
+
+
+/*
+ * Setup and start codes for external interrupts and 
+ * pullup resistors.
+ * 
+ */
+// Enable pull up resistors on pins 18 and 19
+void enablePullups()
+{
+  DDRD = 0;
+  PORTD = 0b00001100;
+  // Use bare-metal to enable the pull-up resistors on pins
+  // 19 and 18. These are pins PD2 and PD3 respectively.
+  // We set bits 2 and 3 in DDRD to 0 to make them inputs. 
+  
+}
+
+ISR(INT2_vect) {
+  rightISR();
+}
+
+ISR(INT3_vect) {
+  leftISR();
+}
+
+// Functions to be called by INT2 and INT3 ISRs.
+void leftISR()
+{
+  leftTicks+= WHEEL_CIRC/8.0;
+  Serial.print("LEFT: ");
+  Serial.println(leftTicks);
+}
+
+void rightISR()
+{
+  rightTicks+= WHEEL_CIRC / 8.0;
+  Serial.print("RIGHT: ");
+  Serial.println(rightTicks);
+}
+
+// Set up the external interrupt pins INT2 and INT3
+// for falling edge triggered. Use bare-metal.
+void setupEINT()
+{
+  EIMSK = 0b00001100;
+  EICRA = 0b10100000;
+  // Use bare-metal to configure pins 18 and 19 to be
+  // falling edge triggered. Remember to enable
+  // the INT2 and INT3 interrupts.
+  // Hint: Check pages 110 and 111 in the ATmega2560 Datasheet.
+
+
+}
+
+// Implement the external interrupt ISRs below.
+// INT3 ISR should call leftISR while INT2 ISR
+// should call rightISR.
+
+
+// Implement INT2 and INT3 ISRs above.
+
+/*
+ * Setup and start codes for serial communications
+ * 
+ */
+// Set up the serial connection. For now we are using 
+// Arduino Wiring, you will replace this later
+// with bare-metal code.
+void setupSerial()
+{
+  // To replace later with bare-metal.
+  Serial.begin(9600);
+  // Change Serial to Serial2/Serial3/Serial4 in later labs when using the other UARTs
+}
+
+// Start the serial connection. For now we are using
+// Arduino wiring and this function is empty. We will
+// replace this later with bare-metal code.
+
+void startSerial()
+{
+  // Empty for now. To be replaced with bare-metal code
+  // later on.
+  
+}
+
+// Read the serial port. Returns the read character in
+// ch if available. Also returns TRUE if ch is valid. 
+// This will be replaced later with bare-metal code.
+
+int readSerial(char *buffer)
+{
+
+  int count=0;
+
+  // Change Serial to Serial2/Serial3/Serial4 in later labs when using other UARTs
+
+  while(Serial.available())
+    buffer[count++] = Serial.read();
+
+  return count;
+}
+
+// Write to the serial port. Replaced later with
+// bare-metal code
+
+void writeSerial(const char *buffer, int len)
+{
+  Serial.write(buffer, len);
+  // Change Serial to Serial2/Serial3/Serial4 in later labs when using other UARTs
+}
+
+/*
+ * Alex's setup and run codes
+ * 
+ */
+
+// Clears all our counters
+void clearCounters()
+{
+  leftTicks=0;
+  rightTicks=0;
+  leftRevs=0;
+  rightRevs=0;
+  forwardDist=0;
+  reverseDist=0; 
+}
+
+// Clears one particular counter
+void clearOneCounter(int which)
+{
+  switch(which)
+  {
+    case 0:
+      clearCounters();
+      break;
+
+    case 1:
+      leftTicks=0;
+      break;
+
+    case 2:
+      rightTicks=0;
+      break;
+
+    case 3:
+      leftRevs=0;
+      break;
+
+    case 4:
+      rightRevs=0;
+      break;
+
+    case 5:
+      forwardDist=0;
+      break;
+
+    case 6:
+      reverseDist=0;
+      break;
+  }
+}
+// Intialize Alex's internal states
+
+void initializeState()
+{
+  clearCounters();
+}
+
+void handleCommand(TPacket *command)
+{
+  switch(command->command)
+  {
+    // For movement commands, param[0] = distance, param[1] = speed.
+    case COMMAND_FORWARD:
+        sendOK();
+        forward((double) command->params[0], (float) command->params[1]);
+      break;
+
+    /*
+     * Implement code for other commands here.
+     * 
+     */
+        
+    default:
+      sendBadCommand();
+  }
+}
+
+void waitForHello()
+{
+  int exit=0;
+
+  while(!exit)
+  {
+    TPacket hello;
+    TResult result;
+    
+    do
+    {
+      result = readPacket(&hello);
+    } while (result == PACKET_INCOMPLETE);
+
+    if(result == PACKET_OK)
+    {
+      if(hello.packetType == PACKET_TYPE_HELLO)
+      {
+     
+
+        sendOK();
+        exit=1;
+      }
+      else
+        sendBadResponse();
+    }
+    else
+      if(result == PACKET_BAD)
+      {
+        sendBadPacket();
+      }
+      else
+        if(result == PACKET_CHECKSUM_BAD)
+          sendBadChecksum();
+  } // !exit
+}
+
+void setup() {
+  // put your setup code here, to run once:
+
+  cli();
+  setupEINT();
+  setupSerial();
+  startSerial();
+  enablePullups();
+  initializeState();
+  sei();
+}
+
+void handlePacket(TPacket *packet)
+{
+  switch(packet->packetType)
+  {
+    case PACKET_TYPE_COMMAND:
+      handleCommand(packet);
+      break;
+
+    case PACKET_TYPE_RESPONSE:
+      break;
+
+    case PACKET_TYPE_ERROR:
+      break;
+
+    case PACKET_TYPE_MESSAGE:
+      break;
+
+    case PACKET_TYPE_HELLO:
+      break;
+  }
+}
+
+void loop() {
+// Uncomment the code below for Step 2 of Activity 3 in Week 8 Studio 2
+
+ forward(0, 100);
+
+// Uncomment the code below for Week 9 Studio 2
+
+/*
+ // put your main code here, to run repeatedly:
+  TPacket recvPacket; // This holds commands from the Pi
+
+  TResult result = readPacket(&recvPacket);
+  
+  if(result == PACKET_OK)
+    handlePacket(&recvPacket);
+  else
+    if(result == PACKET_BAD)
+    {
+      sendBadPacket();
+    }
+    else
+      if(result == PACKET_CHECKSUM_BAD)
+      {
+        sendBadChecksum();
+      } 
+      
+      */
+}
---
+++ w8s2pi/Alex/robotlib.ino
@@ -0,0 +1,90 @@
+#include <AFMotor.h>
+// Direction values
+typedef enum Tdir
+{
+  STOP,
+  GO,
+  BACK,
+  CCW,
+  CW
+} dir;
+
+// Motor control
+#define FRONT_LEFT   4 // M4 on the driver shield
+#define FRONT_RIGHT  1 // M1 on the driver shield
+#define BACK_LEFT    3 // M3 on the driver shield
+#define BACK_RIGHT   2 // M2 on the driver shield
+
+AF_DCMotor motorFL(FRONT_LEFT);
+AF_DCMotor motorFR(FRONT_RIGHT);
+AF_DCMotor motorBL(BACK_LEFT);
+AF_DCMotor motorBR(BACK_RIGHT);
+
+void move(float speed, int direction)
+{
+  int speed_scaled = (speed/100.0) * 255;
+  motorFL.setSpeed(speed_scaled);
+  motorFR.setSpeed(speed_scaled);
+  motorBL.setSpeed(speed_scaled);
+  motorBR.setSpeed(speed_scaled);
+
+  switch(direction)
+    {
+      case BACK:
+        motorFL.run(BACKWARD);
+        motorFR.run(BACKWARD);
+        motorBL.run(FORWARD);
+        motorBR.run(FORWARD); 
+      break;
+      case GO:
+        motorFL.run(FORWARD);
+        motorFR.run(FORWARD);
+        motorBL.run(BACKWARD);
+        motorBR.run(BACKWARD); 
+      break;
+      case CW:
+        motorFL.run(BACKWARD);
+        motorFR.run(FORWARD);
+        motorBL.run(FORWARD);
+        motorBR.run(BACKWARD); 
+      break;
+      case CCW:
+        motorFL.run(FORWARD);
+        motorFR.run(BACKWARD);
+        motorBL.run(BACKWARD);
+        motorBR.run(FORWARD); 
+      break;
+      case STOP:
+      default:
+        motorFL.run(STOP);
+        motorFR.run(STOP);
+        motorBL.run(STOP);
+        motorBR.run(STOP); 
+    }
+}
+
+void forward(float dist, float speed)
+{
+  move(speed, FORWARD);
+}
+
+void backward(float dist, float speed)
+{
+  move(speed, BACKWARD);
+}
+
+void ccw(float dist, float speed)
+{
+  move(speed, CCW);
+}
+
+void cw(float dist, float speed)
+{
+  move(speed, CW);
+}
+
+void stop()
+{
+  move(0, STOP);
+}
+
